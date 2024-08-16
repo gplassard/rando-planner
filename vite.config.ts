97e8da95c781a2089aea46c02c0c61222c7526db
Gabriel Plassard <2485407+gplassard@users.noreply.github.com>
2024-08-16 23:02:51 +0200

    fix(assets): fix static app assets (#11)

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
-import { defineConfig } from 'vite';
+/* eslint-disable import/no-extraneous-dependencies */
 import viteReact from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
